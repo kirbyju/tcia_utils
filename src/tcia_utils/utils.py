d0c3af189059a21d94ced855d941cffed6fb811b
kirbyju <2379527+kirbyju@users.noreply.github.com>
2023-08-06 14:59:13 +0000

    bug fix, version update

--- src/tcia_utils/utils.py
+++ src/tcia_utils/utils.py
@@ -1,8 +1,21 @@
 import pandas as pd
 
-def searchDf(search_term, column_name=None, dataframe=df,):
+def searchDf(search_term, column_name=None, dataframe=None):
+    if dataframe is None:
+        # If dataframe is not provided, assume the dataframe is named 'df'
+        if 'df' in globals():
+            df = globals()['df']
+        else:
+            raise ValueError("Dataframe variable 'df' not found in the global namespace.")
+    else:
+        df = dataframe
+
     if column_name:
         result = df[df[column_name].astype(str).str.contains(search_term, case=False)]
     else:
         result = df[df.apply(lambda row: any(row.astype(str).str.contains(search_term, case=False)), axis=1)]
     return result
+
+
+
+
