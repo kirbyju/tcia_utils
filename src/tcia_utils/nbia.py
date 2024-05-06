6f520f46ec08f718c79c6b736da3da04e341ef63
Justin Kirby <2379527+kirbyju@users.noreply.github.com>
2024-05-06 14:17:36 -0400

    Added getAdvancedQCSearch
    This is a function for TCIA data curators and is not accessible to end users.

--- src/tcia_utils/nbia.py
+++ src/tcia_utils/nbia.py
@@ -89,7 +89,8 @@ def setApiUrl(endpoint, api_url):
     advancedEndpoints = ["getModalityValuesAndCounts", "getBodyPartValuesAndCounts",
                          "getDicomTags", "getSeriesMetadata2", "getCollectionOrSeriesForDOI",
                          "getCollectionValuesAndCounts", "getCollectionDescriptions",
-                         "getSimpleSearchWithModalityAndBodyPartPaged", "getManufacturerValuesAndCounts"]
+                         "getSimpleSearchWithModalityAndBodyPartPaged", "getManufacturerValuesAndCounts",
+                         "getAdvancedQCSearch"]
 
     if not endpoint in searchEndpoints and not endpoint in advancedEndpoints:
         _log.error(
@@ -325,7 +326,8 @@ def queryData(endpoint, options, api_url, format):
             elif format == "csv":
                 df = pd.DataFrame(data)
                 df.to_csv(endpoint + ".csv")
-                _log.info(f"CSV saved to: {endpoint}.csv")
+                df.to_csv(f"{endpoint}_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M')}.csv")
+                _log.info(f"CSV saved to: {endpoint}_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M')}.csv")
                 return df
             else:
                 return data
@@ -1349,6 +1351,117 @@ def getSimpleSearchWithModalityAndBodyPartPaged(
     except requests.exceptions.RequestException as err:
         _log.error(err)
 
+
+def getAdvancedQCSearch(criteria_values, api_url = "", format = "", input_type = {}):
+    """
+    This function allows TCIA data curators to perform an advanced QC search.  
+    This function will not work for end users of TCIA.
+
+    Parameters:
+    - criteria_values (list of tuples): A list of tuples where each tuple contains a criteria type and its corresponding value.
+        - For example, [("patientID", "12345"), ("studyUID", "67890")].
+        - The criteria type can be one of the following: "collection", "qcstatus", "released", "batchnumber", "complete", "patientID", "submissiondate", "studyUID", "seriesUID", "studyDate", "seriesDesc", "modality", "manufacturer".
+        - The value depends on the criteria type and the input type. For "list" input type, it can be a single value or multiple values separated by commas. For "commaSeperatedList" input type, it should be a list of exact matches. For "contains" input type, it should be a substring to search for. For "dateRange" input type, it should be a date or a range of dates in the format "MM/DD/YYYY".
+    - api_url (str, optional): The base URL for the API. Defaults to an empty string.
+    - format (str, optional): The format of the response. Defaults to an empty string.
+    - override_input_type (dict, optional): A dictionary where keys are criteria types and values are the new input types. This allows the user to override the default input type for certain criteria types. Defaults to an empty dictionary.
+        - For example, {"patientID": "contains", "studyUID": "contains"}.
+
+    Returns:
+    - data: The response from the API call.
+    
+    Examples:
+    
+    1. Searching for all patient IDs that contain “LIDC”, have a modality of “CR” or “DX”, 
+    and a submission date between Feb 25, 2020 and Jan 1, 2023. Uses input_type to override
+    the default for patientID (comma separated list) to return anything that contains "LIDC".
+    
+        criteria_values = [("patientID", "LIDC"),
+                            ("modality", "CR,DX"), 
+                            ("submissiondate", "2/25/2020"), 
+                            ("submissiondate", "1/1/2023")]
+
+        input_type = {"patientID": "contains"}
+
+        getAdvancedQCSearch(criteria_values, input_type=input_type)
+
+    2. Searching for all data with collection “APOLLO-5-KIRP//UVA-Limited” or “APOLLO-5-LIHC//UVA-Limited” 
+    and qcstatus is either “Not Visible” or “Visible” with results formatted as a dataframe.
+    
+        criteria_values = [("collection", "APOLLO-5-KIRP//UVA-Limited"),
+                            ("collection", "APOLLO-5-LIHC//UVA-Limited"),
+                            ("qcstatus", "Not Visible"),
+                            ("qcstatus", "Visible")]
+
+        getAdvancedQCSearch(criteria_values, format = "df")
+
+    3. Searching for a list of UIDs with results formatted as a dataframe and saved to a CSV file.
+    
+        criteria_values = [("seriesUID", "1.3.6.1.4.1.14519.5.2.1.6279.6001.709632090821449989953075380984,1.3.6.1.4.1.14519.5.2.1.6279.6001.109097931021726413867023009234")]
+
+        getAdvancedQCSearch(criteria_values, format = "csv")
+    """
+    # Mapping between criteriaType and inputType
+    input_type_map = {
+        "qcstatus": "list",
+        "released": "list",
+        "batchnumber": "list",
+        "complete": "list",
+        "patientID": "commaSeperatedList",
+        "submissiondate": "dateRange",
+        "studyUID": "commaSeperatedList",
+        "seriesUID": "commaSeperatedList",
+        "studyDate": "dateRange",
+        "seriesDesc": "contains",
+        "modality": "list",
+        "manufacturer": "list"
+    }
+    
+    endpoint = "getAdvancedQCSearch"
+
+    # set base_url and headers
+    base_url = setApiUrl(endpoint, api_url)
+
+    params = {}
+    for i, (criteria, value) in enumerate(criteria_values):
+        params[f"criteriaType{i}"] = criteria
+        # Check if the criteria has an override input type
+        if criteria in input_type:
+            params[f"inputType{i}"] = input_type[criteria]
+        else:
+            params[f"inputType{i}"] = input_type_map[criteria]
+        params[f"boolean{i}"] = "AND"
+        params[f"value{i}"] = value
+    
+    # full url
+    url = base_url + endpoint
+    _log.info(f'Calling... {url}')
+    data = requests.post(url, headers=api_call_headers, data=params)
+
+    if data.status_code == 200:
+        # check for empty results and format output
+        if data.text != "":
+            data = data.json()
+            # format the output (optional)
+            if format == "df":
+                df = pd.DataFrame(data)
+                return df
+            elif format == "csv":
+                df = pd.DataFrame(data)
+                df.to_csv(endpoint + ".csv")
+                df.to_csv(f"{endpoint}_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M')}.csv")
+                _log.info(f"CSV saved to: {endpoint}_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M')}.csv")
+                return df
+            else:
+                return data
+        else:
+            _log.info("No results found.")
+    else:
+        _log.error(f"Request failed with status code {data.status_code}")
+        raise Exception(f"Request failed with status code {data.status_code}")
+
+
+        
 ##########################
 ##########################
 # Reports
